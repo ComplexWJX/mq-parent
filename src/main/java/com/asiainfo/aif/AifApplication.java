517e76a2e7f4e71752263f79c55a25faa38b10c8
wengjx <weng.jiaxing@iwhalecloud.com>
2022-06-13 09:32:30 +0800

    结构调整

--- src/main/java/com/asiainfo/aif/AifApplication.java
+++ src/main/java/com/asiainfo/aif/AifApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@ComponentScan(basePackages = {"com.asiainfo.aif.jms"})
 public class AifApplication {
 
     public static void main(String[] args) {
