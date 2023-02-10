215dceed97ad88215fd87074889f2a7ff570394d
wengjx <weng.jiaxing@iwhalecloud.com>
2023-02-10 14:32:57 +0800

    1.测试创建主题，删除主题； 2.消费者消费过主题后，删除该主题，kafka无法启动

--- kafka/src/main/java/com/asiainfo/kafka/admin/AdminClientTool.java
+++ kafka/src/main/java/com/asiainfo/kafka/admin/AdminClientTool.java
@@ -1,9 +1,19 @@
 package com.asiainfo.kafka.admin;
 
 import org.apache.kafka.clients.admin.AdminClient;
-import org.junit.Test;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.DeleteTopicsResult;
+import org.apache.kafka.clients.admin.DescribeTopicsResult;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.TopicDescription;
+import org.apache.kafka.clients.admin.TopicListing;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 /**
  * [一句话描述类功能]
@@ -13,21 +23,77 @@ import java.util.Properties;
  */
 public class AdminClientTool {
 
-    @Test
-    public void testCreateTopic() {
-        createTopic("", "");
-    }
-
-
-    public static void createTopic(String topicName, String partition) {
+    public static void createTopic(String topicName, int partition, short replicationFactors) {
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "localhost:9092");
-        properties.setProperty("topic", topicName);
         // 设置发送主题、分区
-        properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        properties.setProperty("num.partitions", partition);
 
-        AdminClient.create(properties);
+        NewTopic newTopic = new NewTopic(topicName, partition, replicationFactors);
+
+        AdminClient adminClient = AdminClient.create(properties);
+        CreateTopicsResult result = adminClient.createTopics(Collections.singleton(newTopic));
+
+        try {
+            result.all().get();
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println(String.format("create topic %s failed.", topicName));
+        }
+
+        System.out.println(String.format("create topic %s success.", topicName));
+    }
+
+    public static void deleteTopic(String topicName) {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+
+        AdminClient adminClient = AdminClient.create(properties);
+
+
+        /*
+        * 没有在server.properties配置：delete.topic.enable=true，只是标记为 marked for deletion
+        * 彻底删除：
+        *   1.delete.topic.enable=true，重启删除；
+        *   2.zkCli登录，deleteall /brokers/topics/{topic name}；
+        *   3.删除kafka的logs目录下topic相关文件
+        * */
+        DeleteTopicsResult result = adminClient.deleteTopics(Collections.singleton(topicName));
+
+        try {
+            result.all().get();
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println(String.format("delete topic %s failed.", topicName));
+        }
+
+        System.out.println(String.format("delete topic %s success.", topicName));
+    }
+
+    public static Collection<TopicListing> listTopics() {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+
+        AdminClient adminClient = AdminClient.create(properties);
+
+        ListTopicsResult listTopicsResult = adminClient.listTopics();
+
+        try {
+            return listTopicsResult.listings().get();
+        } catch (InterruptedException | ExecutionException e) {
+            return Collections.emptyList();
+        }
+    }
+
+    public static Map<String, TopicDescription> describleTopics(String topicName) {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+
+        AdminClient adminClient = AdminClient.create(properties);
+
+        DescribeTopicsResult describeTopicsResult = adminClient.describeTopics(Collections.singleton(topicName));
+
+        try {
+            return describeTopicsResult.allTopicNames().get();
+        } catch (InterruptedException | ExecutionException e) {
+            return Collections.emptyMap();
+        }
     }
 }
--- kafka/src/test/java/com/asiainfo/kafka/KafkaApplicationTests.java
+++ kafka/src/test/java/com/asiainfo/kafka/KafkaApplicationTests.java
@@ -1,5 +1,8 @@
 package com.asiainfo.kafka;
 
+import com.asiainfo.kafka.admin.AdminClientTool;
+import org.apache.kafka.clients.admin.TopicDescription;
+import org.apache.kafka.clients.admin.TopicListing;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -9,14 +12,45 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Properties;
 
 @SpringBootTest
 class KafkaApplicationTests {
 
+    public static void main(String[] args) {
+        new KafkaApplicationTests().testListTopic();
+    }
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testCreateTopic() {
+        AdminClientTool.createTopic("my-partition-topic1", 3, (short) 1);
+    }
+
+    @Test
+    public void testDeleteTopic() {
+        AdminClientTool.deleteTopic("my-partition-topic1");
+    }
+
+    @Test
+    public void testListTopic() {
+        Collection<TopicListing> topicListings = AdminClientTool.listTopics();
+        System.out.println(topicListings);
+    }
+
+    @Test
+    public void testDescribleTopics() {
+        Map<String, TopicDescription> descriptionMap = AdminClientTool.describleTopics("my-partition-topic");
+        System.out.println(descriptionMap);
     }
 
     @Test
@@ -32,13 +66,14 @@ class KafkaApplicationTests {
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         //使用不同的组名
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "testgroup2");
+
         KafkaConsumer<Integer, String> consumer = new KafkaConsumer<>(props);
         String topic = "order";
         //指定从offset==0开始消费
         int offset = 0;
         int partition = 1;
         TopicPartition topicPartition = new TopicPartition(topic, partition);
-        /**
+        /*
          * 1.standalone consumer:指consumer.assign()而非consumer.subscribe()的消费者
          *      consumer group:指consumer.subscribe的消费者
          *      consumer.assign()和consumer.subscribe()不能同时使用
@@ -47,12 +82,12 @@ class KafkaApplicationTests {
          * standalone consumer和consumer.subscribe()的groupId一样，这里指定的是testgroup2
          */
 
-        consumer.assign(Arrays.asList(topicPartition));
+        consumer.assign(Collections.singletonList(topicPartition));
         consumer.seek(new TopicPartition(topic, partition), offset);
 
         // consumer.subscribe(Collections.singletonList(topic));
         while (true) {
-            ConsumerRecords<Integer, String> records = consumer.poll(1);
+            ConsumerRecords<Integer, String> records = consumer.poll(Duration.of(1, ChronoUnit.MILLIS));
             for (ConsumerRecord<Integer, String> record : records) {
                 System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
             }
